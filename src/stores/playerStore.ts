d0cabce2c020f05d4a9156a8a31b371d3afcfb07
yazanfirst <engyazannmer26@gmail.com>
2025-12-10 05:27:18 -0800

    Add jumpCounter and incrementJump to playerStore

--- src/stores/playerStore.ts
+++ src/stores/playerStore.ts
@@ -5,11 +5,17 @@ type PlayerState = {
   cameraRotation: { azimuth: number; polar: number };
   setPosition: (position: [number, number, number]) => void;
   setCameraRotation: (rotation: { azimuth: number; polar: number }) => void;
+    jumpCounter: number;
+  incrementJump: () => void;
 };
 
 export const usePlayerStore = create<PlayerState>((set) => ({
   position: [0, 0.25, 35],
   cameraRotation: { azimuth: 0, polar: Math.PI / 4 },
+    jumpCounter: 0,
+  incrementJump: () =>
+       set((state) => ({ jumpCounter: state.jumpCounter + 1 })),
+
   setPosition: (position) => set({ position }),
   setCameraRotation: (cameraRotation) => set({ cameraRotation }),
 }));
